262e4df5fe50c42d57a339ccb8316548f6df7867
Shrey Patel <shreypatel@Shreys-MacBook-Air.local>
2024-03-22 21:57:25 +0530

    feat : studemt management

---
+++ ems-backend/src/main/java/net/fernandosalas/ems/dto/DepartmentDto.java
@@ -0,0 +1,42 @@
+package net.fernandosalas.ems.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DepartmentDto {
+    private Long id;
+
+    private String departmentName;
+
+    private String departmentDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+
+    public void setDepartmentDescription(String departmentDescription) {
+        this.departmentDescription = departmentDescription;
+    }
+}
